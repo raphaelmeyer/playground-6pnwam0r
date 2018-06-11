db8776635c89a7ca4ce33c08bad19349b91d182e
Raphael Meyer <raphael.akira.meyer@gmail.com>
2018-06-11 11:24:20 +0200

    return failure

--- cmake-project/00_project_root/check.sh
+++ cmake-project/00_project_root/check.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+set -o pipefail
+
 echo "! $(pwd) > mkdir -p ${DIR}/build"
 mkdir -p ${DIR}/build
 
@@ -10,11 +12,17 @@ cd ${DIR}/build
 
 echo "! $(pwd) > cmake -G Ninja ${DIR}"
 cmake -G Ninja ${DIR} | sed 's/^/    /'
+if [ $? -ne 0 ]; then
+  echo "TECHIO> success false"
+fi
 
 echo "! $(pwd) > cd ${DIR}"
 cd ${DIR}
 
 echo "! $(pwd) > cmake --build ${DIR}/build"
 cmake --build ${DIR}/build | sed 's/^/    /'
+if [ $? -ne 0 ]; then
+  echo "TECHIO> success false"
+fi
 
 echo "TECHIO> success true"
