5fc49d63fa4fd0777a3ed78bba9aee0ec8dad8d5
Raphael Meyer <raphael.akira.meyer@gmail.com>
2018-06-12 15:03:55 +0200

    fix typos

--- cmake-project/02_interface_lib/stubs/usage_check.cmake
+++ cmake-project/02_interface_lib/stubs/usage_check.cmake
@@ -1,5 +1,5 @@
-add_libray(usage_check_1 usage_check.cc)
-target_link_librarys(usage_check PRIVATE time_machine::time_travel)
+add_library(usage_check_1 usage_check.cc)
+target_link_libraries(usage_check PRIVATE time_machine::time_travel)
 
-add_libray(usage_check_2 usage_check.cc)
-target_link_librarys(usage_check PRIVATE time_travel)
+add_library(usage_check_2 usage_check.cc)
+target_link_libraries(usage_check PRIVATE time_travel)
