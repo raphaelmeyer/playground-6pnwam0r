d792aadf5b8d34c185d03b49e0a5a4c51a822b32
Raphael Meyer <raphael.akira.meyer@gmail.com>
2018-06-12 08:59:04 +0200

    rename project folder again

--- cmake-project/01_project_root/check.sh
+++ cmake-project/01_project_root/check.sh
@@ -11,7 +11,7 @@ run_cmake
 cd ${DIR}
 
 if ! grep "cmake_minimum_required" CMakeLists.txt | grep -q -E "VERSION[[:space:]]*3.10" ; then
-  fail 1 "Required minimum CMake version 3.10 is missing."
+  fail 1 "Minimum requirement for CMake version 3.10 is missing."
 fi
 
 if ! grep "project" CMakeLists.txt | grep -q -E "TimeMachine" ; then
