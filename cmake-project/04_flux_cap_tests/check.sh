1dbe817cfe4147701bf64d1aeaef9f437f2b0445
Raphael Meyer <raphael.akira.meyer@gmail.com>
2018-06-13 14:12:52 +0200

    try to match test output

--- cmake-project/04_flux_cap_tests/check.sh
+++ cmake-project/04_flux_cap_tests/check.sh
@@ -26,7 +26,8 @@ run_cmake ${DIR}/project
 cd ${DIR}/project
 out=$(cmake --build {DIR}/project/build --target run-flux_capacitor-tests 2>&1)
 if [[ ! "${out}" =~ "All tests passed (5 assertions in 3 test cases)" ]] ; then
-  fail 1 "Test were not run."
+  echo "TECHIO> message -c DEBUG ${out}"
+  fail 1 "No or not all tests were not run."
 fi
 
 
