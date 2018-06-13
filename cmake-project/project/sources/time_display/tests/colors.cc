26c5f6e8790f989009c161e5ae6bb222cb45ceaa
Raphael Meyer <raphael.akira.meyer@gmail.com>
2018-06-13 16:45:19 +0200

    try workaround

--- cmake-project/project/sources/time_display/tests/colors.cc
+++ cmake-project/project/sources/time_display/tests/colors.cc
@@ -6,9 +6,9 @@ namespace {
 
 TEST_CASE("there should be colors", "[display]" ) {
   Colors const colors;
-  std::variant<Red, Yellow, Green> const red = Red{};
-  std::variant<Red, Yellow, Green> const yellow = Yellow{};
-  std::variant<Red, Yellow, Green> const green = Green{};
+  std::variant<Red, Yellow, Green> red = Red{};
+  std::variant<Red, Yellow, Green> yellow = Yellow{};
+  std::variant<Red, Yellow, Green> green = Green{};
   REQUIRE(colors.update(yellow));
   REQUIRE(not colors.update(red));
   REQUIRE(not colors.update(green));
