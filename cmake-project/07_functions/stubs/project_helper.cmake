48d08391d117c1f02ec9272d0777e24c1bbd54b6
raphael meyer <raphael.akira.meyer@gmail.com>
2018-06-14 22:56:36 +0200

    exercise 7

--- cmake-project/07_functions/stubs/project_helper.cmake
+++ cmake-project/07_functions/stubs/project_helper.cmake
@@ -1,5 +1,9 @@
 # [project]/project_helper.cmake
 
+#
+#
+#
+#
 function(add_interface NAME)
   cmake_parse_arguments(IFACE "" "PACKAGE" "INCLUDES" ${ARGN})
 
@@ -9,8 +13,12 @@ function(add_interface NAME)
   target_include_directories(${NAME} INTERFACE ${IFACE_INCLUDES})
 endfunction(add_interface)
 
+#
+#
+#
+#
 function(add_component NAME)
-  cmake_parse_arguments(COMP "" "PACKAGE" "INCLUDES;SOURCES;SHARES" ${ARGN})
+  cmake_parse_arguments(COMP "" "PACKAGE" "INCLUDES;SOURCES;SHARES;TESTS;USES" ${ARGN})
 
   add_library(${NAME} ${COMP_SOURCES})
   add_library(${COMP_PACKAGE}::${NAME} ALIAS ${NAME})
@@ -23,11 +31,44 @@ function(add_component NAME)
     target_link_libraries(${NAME} PUBLIC ${COMP_SHARES})
   endif()
 
+  if(DEFINED COMP_USES)
+    target_link_libraries(${NAME} PRIVATE ${COMP_USES})
+  endif()
+
+  if(DEFINED COMP_TESTS)
+    add_executable(${NAME}-tests ${COMP_TESTS})
+    add_executable(tests::${NAME} ALIAS ${NAME}-tests)
+
+    add_custom_target(run-${NAME}-tests ALL COMMAND tests::${NAME})
+
+    target_link_libraries(${NAME}-tests
+      PRIVATE
+        ${COMP_PACKAGE}::${NAME}
+        ${COMP_USES}
+        project::settings
+        catchorg::catch2
+    )
+    target_include_directories(${NAME}-tests PRIVATE src)
+  endif()
+
   target_link_libraries(${NAME} PRIVATE project::settings)
 endfunction(add_component)
 
+#
+#
+#
+#
 function(add_application NAME)
-  # ...
-  #
-  #
+  cmake_parse_arguments(APP "" "PACKAGE" "SOURCES;COMPONENTS" ${ARGN})
+
+  add_executable(${NAME} ${APP_SOURCES})
+  add_executable(${APP_PACKAGE}::${NAME} ALIAS ${NAME})
+
+  if(DEFINED APP_COMPONENTS)
+    target_link_libraries(${NAME}
+      PRIVATE
+        ${APP_COMPONENTS}
+        project::settings
+    )
+  endif()
 endfunction(add_application)
