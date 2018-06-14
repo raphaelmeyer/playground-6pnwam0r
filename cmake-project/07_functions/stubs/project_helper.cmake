8b9ab87447f3568418e0d5396533d98de4e4f36c
Raphael Meyer <raphael.akira.meyer@gmail.com>
2018-06-14 15:18:05 +0200

    working on exercise 7

--- cmake-project/07_functions/stubs/project_helper.cmake
+++ cmake-project/07_functions/stubs/project_helper.cmake
@@ -1,18 +1,32 @@
 # [project]/project_helper.cmake
 
-function(add_interface)
-  cmake_parse_arguments(ARG "" "PACKAGE" "INCLUDES" ${ARGN})
-  # ...
-  #
+function(add_interface NAME)
+  cmake_parse_arguments(IFACE "" "PACKAGE" "INCLUDES" ${ARGN})
+
+  add_library(${NAME} INTERFACE)
+  add_library(${IFACE_PACKAGE}::${NAME} ALIAS ${NAME})
+
+  target_include_directories(${NAME} INTERFACE ${IFACE_INCLUDES})
 endfunction(add_interface)
 
-function(add_component)
-  # cmake_parse_arguments(...)
-  # ...
-  #
+function(add_component NAME)
+  cmake_parse_arguments(COMP "" "PACKAGE" "INCLUDES;SOURCES;SHARES" ${ARGN})
+
+  add_library(${NAME} ${COMP_SOURCES})
+  add_library(${COMP_PACKAGE}::${NAME} ALIAS ${NAME})
+
+  if(DEFINED COMP_INCLUDES)
+    target_include_directories(${NAME} PUBLIC ${COMP_INCLUDES})
+  endif()
+
+  if(DEFINED COMP_SHARES)
+    target_link_libraries(${NAME} PUBLIC ${COMP_SHARES})
+  endif()
+
+  target_link_libraries(${NAME} PRIVATE project::settings)
 endfunction(add_component)
 
-function(add_application)
+function(add_application NAME)
   # ...
   #
   #
